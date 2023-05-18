ccaa4fc001f4c6f6e30e73d853f579a54fb85f2c
Igor Galelyuka <galelyuka@gmail.com>
2023-05-18 21:27:48 +0300

    0518 2

--- lng/lng_main.js
+++ lng/lng_main.js
@@ -4,11 +4,78 @@ const mainText = {
     ua: "Відділ перетворювачів форми інформації",
   },
   "header-institute": {
-    en: "V.M.Glushkov's Institute of Ctbernetics",
+    en: "V.M.Glushkov's Institute of Cybernetics",
     ua: "Інститут кібернтики імені В.М.Глушкова",
   },
   "header-nasu": {
     en: "National Academy of Sciences of Ukraine",
     ua: "Національна академія наук України",
   },
+  "nav-main": {
+    en: "Main",
+    ua: "Головна",
+  },
+  "nav-department": {
+    en: "About department",
+    ua: "Про відділ",
+  },
+  "nav-institute": {
+    en: "About institute",
+    ua: "Про інститут",
+  },
+  "nav-projects": {
+    en: "Projects",
+    ua: "Проекти",
+  },
+  "nav-staff": {
+    en: "Staff",
+    ua: "Співробітники",
+  },
+  "nav-publications": {
+    en: "Publications",
+    ua: "Публікації",
+  },
+  "nav-contacts": {
+    en: "Contacts",
+    ua: "Контакти",
+  },
+  "content-welcome": {
+    en: "Welcome to the Site",
+    ua: "Вітаємо на сайті",
+  },
+  "content-welcome1": {
+    en: "Welcome to the website of Data Acquisition System Department of V.M.Glushkov's Institute of Cybernetics of NAS of Ukraine.",
+    ua: "Вітаємо на сайті Відділу перетворювачів форми інформації Інституту кібернетики імені В.М. Глушкова НАН України.",
+  },
+  "content-institute": {
+    en: "More information about our Institute can be found at",
+    ua: "Більше інформації про наш Інститут можна знайти на",
+  },
+  "content-nasu": {
+    en: "More information about National Academy of sciences of Ukraine can be found at",
+    ua: "Більше інформації про Національну академію наук України можна знайти на",
+  },
+  "content-recent-projects": {
+    en: "Recent projects",
+    ua: "Останні проекти",
+  },
+  "content-projects-1-title": {
+    en: "Portable device 'FLORATEST'",
+    ua: "Портативний прилад 'ФЛОРАТЕСТ'",
+  },
+  "content-projects-2-title": {
+    en: "Wireless sensor network for agriculture",
+    ua: "Бездротова сенсорна мережа для сільського господарства",
+  },
+  "content-projects-3-title": {
+    en: "Medical communicator",
+    ua: "Медичний комунікатор",
+  },
 };
+
+/*
+"":{
+  en: "",
+  ua: "",
+},
+*/
