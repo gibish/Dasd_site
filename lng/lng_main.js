4a65c84c4f612de181e390e590fbe6cdd5b25b95
Igor Galelyuka <galelyuka@gmail.com>
2023-06-03 22:06:42 +0300

    0603 1

--- lng/lng_main.js
+++ lng/lng_main.js
@@ -93,7 +93,7 @@ const mainText = {
   },
   "footer-department": {
     en: "Acquisition System Department of V.M.Glushkov's Institute of Cybernetics of NAS of Ukraine",
-    ua: "Відділ перетворювачів форми інформації Інституту кібернетики імені В.М. Глушкова НАН України",
+    ua: "Відділ перетворювачів форми інформації Інституту кібернетики імені В.М.Глушкова НАН України",
   },
   "footer-created": {
     en: "Created by",
