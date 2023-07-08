4255572a6b8caf235ad48115edfeeb1a777d1f42
Igor Galelyuka <galelyuka@gmail.com>
2023-07-08 21:35:58 +0300

    0708 1

--- lng/lng_department.js
+++ lng/lng_department.js
@@ -3,6 +3,22 @@ const departmentText = {
     en: "Data acquisition system department",
     ua: "Відділ перетворювачів форми інформації",
   },
+  "dept-content-welcome": {
+    en: "About Data Acquisition System Department",
+    ua: "Про відділ перетворювачів форми інформації",
+  },
+  "dept-content-1": {
+    en: "The department was founded by doctor of sciences, professor A.I. Kondalev in 1963, who was at the head till 1989. There are 21 scientists in the department now including 4 Doctors of sciences, and 4 PhDs.",
+    ua: "Відділ засновано 1963 року доктором технічних наук, професором А.І. Кондалєвим, який очолював його до 1989 року. У відділі працюють 21 наукових співробітників, серед них – 4 доктори та 4 кандидати наук.",
+  },
+  "dept-content-2": {
+    en: "Now the department is headed by doctor of sciences, professor V.O. Romanov.",
+    ua: "Зараз відділ очолює доктор технічних наук, професор В.О. Романов.",
+  },
+  "dept-content-head": {
+    en: "Volodymyr ROMANOV, Head of department, Doctor of technical sciences, Professor. He was awarded Academician Lebedev Prize, Honorary degree Inventor of the Year of Ukraine, awarded State Prize of Ukraine in Science and Technique, and Honorary degree Honoured Scientist of Ukraine.",
+    ua: "РОМАНОВ Володимир Олександрович, Завідувач відділу, доктор технічних наук, професор. заслужений діяч науки і техніки України, лауреат премії С.О. Лебедєва, лауреат Державної премії в галузі науки і техніки України.",
+  },
 };
 
 /*
