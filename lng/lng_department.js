1c388566d45c393aef36a7f05652b8fd1622476a
Igor Galelyuka <galelyuka@gmail.com>
2023-07-09 22:13:38 +0300

    0709 1

--- lng/lng_department.js
+++ lng/lng_department.js
@@ -15,9 +15,37 @@ const departmentText = {
     en: "Now the department is headed by doctor of sciences, professor V.O. Romanov.",
     ua: "Зараз відділ очолює доктор технічних наук, професор В.О. Романов.",
   },
-  "dept-content-head": {
-    en: "Volodymyr ROMANOV, Head of department, Doctor of technical sciences, Professor. He was awarded Academician Lebedev Prize, Honorary degree Inventor of the Year of Ukraine, awarded State Prize of Ukraine in Science and Technique, and Honorary degree Honoured Scientist of Ukraine.",
-    ua: "РОМАНОВ Володимир Олександрович, Завідувач відділу, доктор технічних наук, професор. заслужений діяч науки і техніки України, лауреат премії С.О. Лебедєва, лауреат Державної премії в галузі науки і техніки України.",
+  "dept-content-head-name": {
+    en: "Volodymyr ROMANOV",
+    ua: "РОМАНОВ Володимир Олександрович",
+  },
+  "dept-content-head-position": {
+    en: "Head of department",
+    ua: "Завідувач відділу",
+  },
+  "dept-content-head-degree": {
+    en: "Doctor of technical sciences, Professor",
+    ua: "Доктор технічних наук, професор",
+  },
+  "dept-content-head-awards": {
+    en: "Awarded Academician Lebedev Prize, Honorary degree Inventor of the Year of Ukraine, awarded State Prize of Ukraine in Science and Technique, and Honorary degree Honoured Scientist of Ukraine.",
+    ua: "Заслужений діяч науки і техніки України, лауреат премії С.О. Лебедєва, лауреат Державної премії в галузі науки і техніки України.",
+  },
+  "dept-content-3": {
+    en: "The main directions of scientific activity",
+    ua: "Основні напрями наукової діяльності",
+  },
+  "dept-content-3-1": {
+    en: "theory and methodology of creation of CAD/CAM information technologies for creation of computer means and systems for data converting, processing, transferring, and visualization;",
+    ua: "теоретичні основи та методологія створення інформаційних технологій автоматизованого проектування комп'ютерних засобів та систем для перетворення, обробки, передавання та візуалізації інформації;",
+  },
+  "dept-content-3-2": {
+    en: "methodology of creation and application of wireless sensor networks for express diagnostics of state of biological and physical objects;",
+    ua: "методологія створення та впровадження бездротових засобів та бездротових сенсорних мереж для експрес-діагностики стану біологічних та фізичних об'єктів;",
+  },
+  "dept-content-3-3": {
+    en: "theory and methodology of processing results of multiple measurement conversations of physical quantity, theory of redundant measurements, and metrological reliability of means of redundant measurements.",
+    ua: "теорія та методи обробки результатів багатократних вимірювальних перетворень фізичної величини, теорія надлишкових вимірювань та метрологічної надійності засобів надлишкових вимірювань.",
   },
 };
 
