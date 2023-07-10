3869b3e5abeda747de673708bec2af915b78f422
Igor Galelyuka <galelyuka@gmail.com>
2023-07-10 21:54:35 +0300

    0710 1

--- lng/lng_department.js
+++ lng/lng_department.js
@@ -47,6 +47,34 @@ const departmentText = {
     en: "theory and methodology of processing results of multiple measurement conversations of physical quantity, theory of redundant measurements, and metrological reliability of means of redundant measurements.",
     ua: "теорія та методи обробки результатів багатократних вимірювальних перетворень фізичної величини, теорія надлишкових вимірювань та метрологічної надійності засобів надлишкових вимірювань.",
   },
+  "dept-content-4": {
+    en: "Some fundamental results",
+    ua: "Деякі фундаментальні результати",
+  },
+  "dept-content-4-1": {
+    en: "it was developed theory and methodology of design of high-productive means of data converting, processing, transferring, and visualization;",
+    ua: "створено теоретичні основи та методологію проектування високопродуктивних засобів перетворення, обробки, передавання та візуалізації інформації;",
+  },
+  "dept-content-4-2": {
+    en: "it was developed methodology of creating new information technologies for virtual design of computer devices and means for data conversion, processing, transferring, and visualization of state of complicated biological and physical objects;",
+    ua: "розроблено методологію створення нових інформаційних технологій віртуального проектування комп'ютерних приладів та засобів для перетворення, аналізу, обробки, передавання та візуалізації інформації про стан складних біологічних та фізичних об'єктів;",
+  },
+  "dept-content-4-3": {
+    en: "it was developed methodology of creating, deployment and self-organizing of wireless sensor networks for express-diagnostics of state of biological and physical objects in agriculture, ecology, medicine, housing and municipal, and other areas of human activity;",
+    ua: "розроблено методологію створення, розгортання та самоорганізації бездротових сенсорних мереж для експрес-діагностики стану біологічних та фізичних об'єктів у сільському господарстві, екології, медицині, житлово-комунальній сфері та інших сферах діяльності людини;",
+  },
+  "dept-content-4-4": {
+    en: "it was developed methodology of processing the results of multiple measurement conversions of physical quantity of different nature;",
+    ua: "розроблено методологію обробки результатів багатократних вимірювальних перетворень фізичних величин різної природи;",
+  },
+  "dept-content-4-5": {
+    en: "it was developed theory of redundant measurements, and methods of creation of measurement means on this base",
+    ua: "розроблено теоретичні основи надлишкових вимірювань та принципи побудови вимірювальних засобів на їх основі.",
+  },
+  "dept-content-5": {
+    en: "Some applied results",
+    ua: "Деякі прикладні результати",
+  },
 };
 
 /*
