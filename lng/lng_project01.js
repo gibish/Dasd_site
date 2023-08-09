aeb6272d91e3b14916aa1e6953d9eceea2b62236
Igor Galelyuka <galelyuka@gmail.com>
2023-08-09 15:02:32 +0300

    0809 1

--- lng/lng_project01.js
+++ lng/lng_project01.js
@@ -3,11 +3,7 @@ const project01Text = {
     en: "Portable device 'FLORATEST'",
     ua: "Портативний прилад 'ФЛОРАТЕСТ'",
   },
-  "proj-name-1": {
-    en: "Portable device 'FLORATEST'",
-    ua: "Портативний прилад 'ФЛОРАТЕСТ'",
-  },
-  "proj-descr-1": {
+  "proj01-descr": {
     en: "The portable device 'FLORATEST' is intended for express-diagnostics of the impact of stress factors of natural and anthropogenic origin on the state of plants. The device measures the induction of chlorophyll fluorescence without damaging the plant. The use of the curve of chlorophyll fluorescence induction makes it possible to diagnose the influence of one or another factor on the state of plants and to take timely measures to eliminate or compensate the negative impact in order to avoid crop losses or plant death.",
     ua: "Портативний прилад 'ФЛОРАТЕСТ' призначено для експрес-діагностики впливу стресових чинників природного і техногенного походження на стан рослин. Прилад вимірює індукцію флуоресценції хлорофілу без пошкодження рослини. Використання кривої індукції флуоресценції хлорофілу дає можливість діагностувати вплив того чи іншого чиннику на стан рослин та вчасно вжити заходи для усунення або компенсації негативного впливу з метою уникнення втрат урожаю або загибелі рослин.",
   },
