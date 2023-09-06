be9e4cc3416823fd8f4a9c122842e09cdaa7166a
Igor Galelyuka <galelyuka@gmail.com>
2023-09-06 14:51:18 +0300

    0906 1

--- script.js
+++ script.js
@@ -134,3 +134,6 @@ navToggle.addEventListener("click", (event) => {
   navMenu.classList.toggle("nav__menu-active");
   navToggle.classList.toggle("active");
 });
+
+const darkIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Dark"><path d="M10 2c-1.82 0-3.53.5-5 1.35C7.99 5.08 10 8.3 10 12s-2.01 6.92-5 8.65C6.47 21.5 8.18 22 10 22c5.52 0 10-4.48 10-10S15.52 2 10 2z"></path></svg>`;
+const sunriseIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Sunrise"><path d="M20 15.31 23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18V6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`;
