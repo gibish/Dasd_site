b0e0271aa4850ac4d9e51cd2bf8acbe8f5364ba5
Igor Galelyuka <galelyuka@gmail.com>
2023-09-07 15:06:44 +0300

    0907 1

--- script.js
+++ script.js
@@ -136,4 +136,49 @@ navToggle.addEventListener("click", (event) => {
 });
 
 const darkIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Dark"><path d="M10 2c-1.82 0-3.53.5-5 1.35C7.99 5.08 10 8.3 10 12s-2.01 6.92-5 8.65C6.47 21.5 8.18 22 10 22c5.52 0 10-4.48 10-10S15.52 2 10 2z"></path></svg>`;
-const sunriseIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Sunrise"><path d="M20 15.31 23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18V6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`;
+const lightIcon = `<svg fill="currentColor" aria-hidden="true" viewBox="0 0 24 24" tabindex="-1" title="Sunrise"><path d="M20 15.31 23.31 12 20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69zM12 18V6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"></path></svg>`;
+
+let imgLight = document.getElementById("img_light_theme");
+imgLight.innerHTML = lightIcon;
+let imgDark = document.getElementById("img_dark_theme");
+imgDark.innerHTML = darkIcon;
+
+const allThemes = ["light", "dark"];
+//let currentLang = localStorage.getItem("language") || checkBrowserLang() || "en";
+const themeButtons = document.querySelectorAll("[data-theme_btn]");
+console.log(themeButtons);
+
+let currentTheme = "dark";
+
+themeButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    currentTheme = event.target.dataset.theme_btn;
+    //    localStorage.setItem("language", event.target.dataset.btn);
+    resetActiveTheme(themeButtons, "active");
+    btn.classList.add("active");
+    //    changeLang();
+    console.log(currentTheme);
+  });
+});
+
+function resetActiveTheme(arr, activeClass) {
+  arr.forEach((elem) => {
+    elem.classList.remove(activeClass);
+  });
+}
+
+function checkActiveThemeButton() {
+  switch (currentTheme) {
+    case "light":
+      document.querySelector(`[data-theme_btn="light"]`).classList.add("active");
+      break;
+    case "dark":
+      document.querySelector(`[data-theme_btn="dark"]`).classList.add("active");
+      break;
+    default:
+      document.querySelector(`[data-theme_btn="dark"]`).classList.add("active");
+      break;
+  }
+}
+
+checkActiveThemeButton();
